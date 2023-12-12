a84827c57ebbcb14d367f57d79d92aa5158d0e6c
Jesper Ekerling <jesper@forme.se>
2023-12-12 10:07:21 +0100

    Adding first test

--- summa.js
+++ summa.js
@@ -1,23 +1,5 @@
 // MinFuntion.js
-function minFunktion(a, b) {
+function beraknaSumma(a, b) {
     return a + b;
   }
-  module.exports = minFunktion;
-  
-  
-  // MinFunktion.test.js
-  const minFunktion = require('./MinFunktion');
-  
-  describe('MinFunktion', () => {
-    it('returnerar rätt värde', () => {
-      const resultat = minFunktion(2, 3);
-      expect(resultat).toBe(5);
-    });
-  
-    it('hanterar negativa tal', () => {
-      const resultat = minFunktion(-5, 3);
-      expect(resultat).toBe(-2);
-    });
-  });
-
-  
+  module.exports = beraknaSumma;
--- summa.test.js
+++ summa.test.js
@@ -1,1 +1,9 @@
-beraknaSumma(a, b)
+const beraknaSumma = require('./summa');
+
+
+describe('beraknaSumma', () => {
+    it('Beräknar summan av två positiva tal', () => {
+        expect(beraknaSumma(1, 2)).toBe(3);
+        })
+    })
+
