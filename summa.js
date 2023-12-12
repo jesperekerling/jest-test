6227f8a616142dd1845b08918a4859bafe8eaeb3
Jesper Ekerling <jesper@forme.se>
2023-12-12 08:42:47 +0100

    Test of jest

---
+++ summa.js
@@ -0,0 +1,22 @@
+// MinFuntion.js
+function minFunktion(a, b) {
+    return a + b;
+  }
+  module.exports = minFunktion;
+  
+  
+  // MinFunktion.test.js
+  const minFunktion = require('./MinFunktion');
+  
+  describe('MinFunktion', () => {
+    it('returnerar rätt värde', () => {
+      const resultat = minFunktion(2, 3);
+      expect(resultat).toBe(5);
+    });
+  
+    it('hanterar negativa tal', () => {
+      const resultat = minFunktion(-5, 3);
+      expect(resultat).toBe(-2);
+    });
+  });
+  
